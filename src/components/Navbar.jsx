3ce684bd9fb4b2c4638d16346cf9f17610277afc
akhan2106 <124744627+akhan2106@users.noreply.github.com>
2024-11-10 22:26:29 -0800

    Project init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {assets} from '../assets/assets'
 
 const Navbar = () => {
     const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+useEffect(()=>{
+    if(showMobileMenu){
+        document.body.style.overflow='hidden'
+    }else{
+        document.body.style.overflow='auto' 
+    }
+    return () =>{
+        document.body.style.overflow='auto'
+    };
+},[showMobileMenu])
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
         <div className='container mx-auto flex justify-between 
@@ -11,11 +22,11 @@ const Navbar = () => {
             <ul className='hidden md:flex gap-7 text-white'>
                 <a href="#Header" className='cursor-pointer
                 hover:text-gray-400'>Home</a>
-                <a href="#Header" className='cursor-pointer
+                <a href="#About" className='cursor-pointer
                 hover:text-gray-400'>About</a>
-                <a href="#Header" className='cursor-pointer
+                <a href="#Projects" className='cursor-pointer
                 hover:text-gray-400'>Projects</a>
-                <a href="#Header" className='cursor-pointer
+                <a href="#Testimonials" className='cursor-pointer
                 hover:text-gray-400'>Testimonials</a>
             </ul>
             <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
@@ -26,17 +37,17 @@ const Navbar = () => {
         <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0'} fixed w-full right-0 top-0 bottom-0
         overflow-hidden bg-white transition-all`}>
             <div className='flex justify-end p-6 cursor-pointer'>
-                <img src={assets.cross_icon} className='w-6' alt="" />
+                <img onClick={()=> setShowMobileMenu(false)}src={assets.cross_icon} className='w-6' alt="" />
                 </div>
             <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg
             font-medium'>
-                <a href="#Header" className='px-4 py2 rounded-full
+                <a onClick={()=> setShowMobileMenu(false)} href="#Header" className='px-4 py2 rounded-full
                 inline-block'>Home</a>
-                <a href="#About" className='px-4 py2 rounded-full
+                <a onClick={()=> setShowMobileMenu(false)} href="#About" className='px-4 py2 rounded-full
                 inline-block'>About</a>
-                <a href="#Projects" className='px-4 py2 rounded-full
+                <a onClick={()=> setShowMobileMenu(false)} href="#Projects" className='px-4 py2 rounded-full
                 inline-block'>Projects</a>
-                <a href="#Testimonials" className='px-4 py2 rounded-full
+                <a onClick={()=> setShowMobileMenu(false)} href="#Testimonials" className='px-4 py2 rounded-full
                 inline-block'>Testimonials</a>
             </ul>
         </div>
